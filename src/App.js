8842fd54d1281faf3521317bf74d1f33284d61cf
Pankaj Vaishnav <97958495+pankaj-vaishnaw@users.noreply.github.com>
2022-11-10 14:21:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import Card from './components/Card';
+import './App.css';
+
+function App() {
+  
+    const main_styling = {
+      display: "flex",
+      justifyContent: "center",
+      flexWrap: "wrap",
+      padding: "0.1rem",
+    };
+    return (
+      <>
+        <div style={main_styling}>
+          <Card title="#FF6663" color="PINK" hashcode="PINK" />
+          <Card title="#333333" color="GRAY" hashcode="#333333" />
+          <Card title="#000000" color="BLACK" hashcode="#000000" />
+          <Card title="#38BB14" color="GREEN" hashcode="#38BB14" />
+          <Card title="#C90B0B" color="RED" hashcode="#C90B0B" />
+        </div>
+  
+        <div style={main_styling}>
+          <Card title="#FF8000" color="ORANGE" hashcode="#FF8000" />
+          <Card title="#FFf500" color="YELLOW" hashcode="#FFf500" />
+          <Card title="#CCCCCC" color="LIGHT-GRAY" hashcode="#CCCCCC" />
+          <Card title="#7E41A2" color="PURPLE" hashcode="#7E41A2" />
+          <Card title="#C14911" color="BROWN" hashcode="#C14911" />
+        </div>
+      </>
+  );
+}
+
+export default App;
